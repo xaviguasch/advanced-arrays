dfd15d0a291b06bbe8e62fee1e394beb068e3758
xaviguasch <xaviguasch@gmail.com>
2019-04-16 12:47:05 +0200

    added solution

--- exercise5.js
+++ exercise5.js
@@ -1,29 +1,28 @@
 // Complete the below questions using this array:
-const array = [
-  {
-    username: "john",
-    team: "red",
-    score: 5,
-    items: ["ball", "book", "pen"]
-  },
-  {
-    username: "becky",
-    team: "blue",
-    score: 10,
-    items: ["tape", "backpack", "pen"]
-  },
-  {
-    username: "susy",
-    team: "red",
-    score: 55,
-    items: ["ball", "eraser", "pen"]
-  },
-  {
-    username: "tyson",
-    team: "green",
-    score: 1,
-    items: ["book", "pen"]
-  },
+const array = [{
+        username: "john",
+        team: "red",
+        score: 5,
+        items: ["ball", "book", "pen"]
+    },
+    {
+        username: "becky",
+        team: "blue",
+        score: 10,
+        items: ["tape", "backpack", "pen"]
+    },
+    {
+        username: "susy",
+        team: "red",
+        score: 55,
+        items: ["ball", "eraser", "pen"]
+    },
+    {
+        username: "tyson",
+        team: "green",
+        score: 1,
+        items: ["book", "pen"]
+    },
 
 ];
 
@@ -42,9 +41,9 @@ const array = [
 // (2), Make this map function pure:
 const arrayNum = [1, 2, 4, 5, 8, 9];
 const newArray = arrayNum.map((num, i) => {
-	console.log(num, i);
-	alert(num);
-	return num * 2;
+    console.log(num, i);
+    alert(num);
+    return num * 2;
 })
 
 //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
